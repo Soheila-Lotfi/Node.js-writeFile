252f3755ad6e0a414f3faf61a546c001c71d4af4
soheila <soheilalotfi@ymail.com>
2019-07-22 11:50:04 -0400

    change movie.text to movie.txt

--- write.js
+++ write.js
@@ -1,6 +1,6 @@
 var fs=require("fs");  // fs is a node package to read and write files
 
-fs.writeFile("movie.text", "forest gump, inception, harry potter", function(err){       // if there is no movies.txt, it will creat one and "forest gump, inception, harry potter" will be printed in movies.txt
+fs.writeFile("movie.txt", "forest gump, inception, harry potter", function(err){       // if there is no movies.txt, it will creat one and "forest gump, inception, harry potter" will be printed in movies.txt
 
     if (err){
         return console.log(err);   //if the code experiences any error, it will log the erroe to the console
@@ -10,3 +10,4 @@ fs.writeFile("movie.text", "forest gump, inception, harry potter", function(err)
     }
    
 });
+
